7ba171b88f657fade12c9644a83b10feba128d04
Benjamin Williams <bmw@st-andrews.ac.uk>
2016-04-18 11:25:25 +0100

    Undid non-working changes

--- sf.f90
+++ sf.f90
@@ -12,7 +12,7 @@ integer :: nnulls
 integer :: pcount, ncount, ucount
 
 integer :: sign, spiral, warning
-double precision, dimension(3) :: spine,fan
+double precision, dimension(3) :: spine,fan!,rnull
 
 integer :: i
 
@@ -43,6 +43,7 @@ dz = (z(nz)-z(1))/nz
 
 print*, nnulls,' nulls'
 
+
 !now loop over each null and characterise using get_properties
 do i = 1, nnulls
   print*, 'Evaluating null', i,' of', nnulls
@@ -56,6 +57,8 @@ do i = 1, nnulls
   spirals(i) = spiral
   warnings(i) = warning
 enddo
+
+
 
 !now write data to nulls.dat
 open(unit=10,file='output/nulls.dat',form='unformatted')
@@ -94,18 +97,18 @@ subroutine get_properties(sign,spine,fan,spiral,warning)
 use sfmod
 
 implicit none
-integer :: i, j, k, count, maxcount, n
-double precision :: r(3), b(3), rold(3), rnew(3), bnew(3), fact
+integer :: i, j
+double precision :: r(3), b(3)
 double precision :: dphi, dtheta
 double precision :: mn, mx
 integer :: mxloc(2), mnloc(2)
+!double precision :: sep
 integer :: nmax, nmin, nsaddle
 double precision :: minvec(3), maxvec(3)
 double precision :: flux, crossflux
 integer :: sign, spiral, warning
 
 double precision, dimension(3) :: spine, fan!, major, minor
-double precision, dimension(:,:), allocatable :: rconverge1, rconverge2, dum
 
 !set up theta and phi for sphere around null
 dphi = 360.d0/dble(nphi)
@@ -132,8 +135,6 @@ print*, 'B=', trilinear(rnull, bgrid)
 ! thetarot=0.
 ! phirot=0.
 
-allocate(rconverge1(3,nphi*ntheta), rconverge2(3,nphi*ntheta))
-
 !create sphere
 flux = 0
 crossflux = 0
@@ -147,18 +148,15 @@ do j = 1, ntheta
     
     ! calculate different grids
     modb(i,j) = modulus(b) ! size of b on the sphere
-    btotal(i,j) = dot(b,r)/rsphere !=modulus(r) ! b in the radial direction on the sphere's surface
+    btotal(i,j) = dot(b,r)/modulus(r) ! b in the radial direction on the sphere's surface
     bmap(i,j) = btotal(i,j)/modb(i,j) ! angle between b and r
-    bcross(i,j) = modulus(cross(b,r))/modb(i,j)/rsphere !=modulus(r) ! vector orthogonal to b and r
+    bcross(i,j) = modulus(cross(b,r))/modb(i,j)/modulus(r) ! vector orthogonal to b and r
     
     !calculate integrals of flux and normal bcross vector on sphere
-    !flux = flux + abs(btotal(i,j))*dphi*dtheta*sin(thetas(j))
-    !crossflux = crossflux + abs(bcross(i,j))*modb(i,j)*dphi*dtheta*sin(thetas(j)) ! should crossflux be a vector or scalar?
+    flux = flux + abs(btotal(i,j))*dphi*dtheta*sin(thetas(j))
+    crossflux = crossflux + abs(bcross(i,j))*modb(i,j)*dphi*dtheta*sin(thetas(j)) ! should crossflux be a vector or scalar?
   enddo
 enddo
-
-print*, "-----------------------------------------------------------"
-!stop
 
 !print*, 'FLUX=',flux
 !print*,'CROSS',crossflux
@@ -171,27 +169,8 @@ mxloc = maxloc(bmap)
 mn = minval(bmap)
 mx = maxval(bmap)
 
-!print*, sphere2cart(rsphere,thetas(mnloc(2)),phis(mnloc(1)))
-!print*, sphere2cart(rsphere,thetas(mxloc(2)),phis(mxloc(1)))
-!print*,mn
-!print*,mx
-!print*,btotal(mnloc(1),mnloc(2))
-!print*,btotal(mxloc(1),mxloc(2))
-
 !get maxima, minima and saddle points
 call get_maxima(bmap, maxima,minima,saddle, nmax,nmin,nsaddle)
-
-print*, "Global Max:", sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
-print*, "Global Min:", sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
-print*, "Local Max:"
-do i = 1, nmax
-  print*, sphere2cart(1.d0,thetas(maxima(2,i)),phis(maxima(1,i)))
-enddo
-print*, "Local Min:"
-do i = 1, nmax
-  print*, sphere2cart(1.d0,thetas(minima(2,i)),phis(minima(1,i)))
-enddo
-print*, "------------------------------------------------------------------"
 
 sign = 0
 spiral = 0
@@ -202,7 +181,6 @@ if (abs(mn) .gt. spiraltol .and. abs(mx) .gt. spiraltol) then !if both signs hav
   
   if ((nmax .eq. 2) .and. (nmin .gt. 2)) then
     !print*, 'Negative non-spiral null'
-    print*,"Using option 1"
     sign = -1
     spiral = 0
     
@@ -210,9 +188,10 @@ if (abs(mn) .gt. spiraltol .and. abs(mx) .gt. spiraltol) then !if both signs hav
     
     call getminmax(nmin,minima,minvec,maxvec)
     
+    !fan=cross(minvec,maxvec)
+    
   else if ((nmin .eq. 2) .and. (nmax .gt. 2)) then
     !print*, 'Positive non-spiral null'
-    print*,"Using option 2"
     sign = 1
     spiral = 0
     
@@ -221,7 +200,6 @@ if (abs(mn) .gt. spiraltol .and. abs(mx) .gt. spiraltol) then !if both signs hav
     call getminmax(nmax,maxima,minvec,maxvec)
 
   else if ((nmin .eq. 2) .and. (nmax .eq. 2) .and. (nsaddle .eq. 2)) then
-    print*,"Using option 3"
     sign = 1 !pick a ramdom sign (we will test whether this sign is correct later on, and swap it if necessary)
     spiral = 1
     !print*,'Spiral null - indeterminate sign'
@@ -231,66 +209,56 @@ if (abs(mn) .gt. spiraltol .and. abs(mx) .gt. spiraltol) then !if both signs hav
     maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
     minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
     
-  else if ((nmax .eq. 2) .and. (nmin .eq. 1) .and. (nsaddle .eq. 1)) then
-    print*,"Using option 4"
+  else if ( (nmax .eq. 2) .and. (nmin .eq. 1) .and. (nsaddle .eq. 1)) then
     sign = -1
     spiral = 0
       spine = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       maxvec = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
-  else if ((nmax .eq. 1) .and. (nmin .eq. 2) .and. (nsaddle .eq. 1)) then
-    print*,"Using option 5"
+  else if ( (nmax .eq. 1) .and. (nmin .eq. 2) .and. (nsaddle .eq. 1)) then
     sign = 1
     spiral = 0
       spine = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
   else if (nmax .eq. 2 .and. nmin .eq. 2) then
-    print*,"Using option 6"
     sign = 1
     spiral = 0
       spine = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       minvec = normalise(cross(spine,maxvec))
   else
-    print*,"Using option 7"
     sign = 0
     spiral = 0
     print*, "Odd null - can't characterise"
     !stop
   endif
+  
 else
   !print*, 'Spiral null'
   spiral = 1
   
   if ((mx .gt. spiraltol) .and. (abs(mn) .lt. spiraltol)) then
     ! print*, 'Negative Spiral'
-    print*,"Using option 8"
     sign = -1
     spine = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
     
     if ((nmax .eq. 2) .and. (nmin .eq. 2) .and. (nsaddle .eq. 2)) then
-      print*,"Using option 9"
       maxvec = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
     else if ((nmax .eq. 2) .and. (nmin .gt. 2)) then
-      print*,"Using option 10"
       call getminmax(nmin,minima,minvec,maxvec)
     else if ((nmax .ge. 4) .and. (nmin .eq. 2)) then
-      print*,"Using option 11"
       call getminmax(nmax, maxima,minvec,maxvec)
       if (abs(dot(spine,maxvec)) .lt. abs(dot(spine,minvec))) minvec=maxvec
       maxvec=sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
     else if ( (nmax .eq. 2) .and. (nmin .eq. 1) .and. (nsaddle .eq. 1)) then
-      print*,"Using option 12"
       maxvec = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
     else if ( (nmax .eq. 2) .and. (nmin .eq. 2) ) then
-      print*,"Using option 13"
       maxvec = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
       minvec = normalise(cross(spine,maxvec))
     else
-      print*,"Using option 14"
       print*, 'weird null!'
       sign = 0
     endif
@@ -300,32 +268,25 @@ else
     spine = sphere2cart(1.d0,thetas(mnloc(2)),phis(mnloc(1)))
     
     if ((nmax .eq. 2) .and. (nmin .eq. 2) .and. (nsaddle .eq. 2)) then
-      print*,"Using option 15"
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
     else if ((nmax .eq. 2) .and. (nmin .gt. 2)) then
-      print*,"Using option 16"
       call getminmax(nmin,minima,minvec,maxvec)
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
     else if ((nmin .ge. 4) .and. (nmax .eq. 2)) then
-      print*,"Using option 17"
       call getminmax(nmax,maxima,minvec,maxvec)
     else if ( (nmax .eq. 1) .and. (nmin .eq. 2) .and. (nsaddle .eq. 1)) then
-      print*,"Using option 18"
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       minvec = sphere2cart(1.d0,thetas(saddle(2,1)),phis(saddle(1,1)))
     else if ( (nmax .eq. 2) .and. (nmin .eq. 2) ) then
-      print*,"Using option 19"
       maxvec = sphere2cart(1.d0,thetas(mxloc(2)),phis(mxloc(1)))
       minvec = normalise(cross(spine,maxvec))
     else
-      print*,"Using option 20"
       print*, 'weird null!'
       sign = 0
     endif
     
   else
-    print*,"Using option 21"
     sign = 0
     spiral = 1
     print*, 'strange null'
@@ -333,20 +294,13 @@ else
   
  ! stop
   
-endif
+endif 
 
 !if major and minor axes are almost parallel, redefine minor axis as spine x major
 !print*,'CROSS',modulus(cross(maxvec,minvec))
 if (modulus(cross(maxvec,minvec)) .lt. 0.1) then
   minvec = cross(spine,maxvec)
 endif
-
-print*, "Spine:", spine
-print*, "Maxvec:", maxvec
-print*, "Minvec:", minvec
-print*, "Fan:", normalise(cross(minvec,maxvec))
-
-print*, "-----------------------------------------------------------------------"
 
 print*, ''
 print*, "Determining null's properties..."
@@ -356,6 +310,7 @@ call test_null(spine,maxvec,minvec,sign,spiral)
 call test_null(spine,maxvec,minvec,sign,spiral)
 !call test_null(spine,maxvec,minvec,sign,spiral)
 fan = normalise(cross(minvec,maxvec))
+
 
 if (sign .eq. 0) then
   fan = (/0.d0,0.d0,1.d0/)
@@ -367,7 +322,7 @@ print*, 'Spiral =', spiral
 print*, 'Spine = ', spine
 print*, 'Fan =   ', fan
 print*, 'Tilt =  ', abs(90-acos(dot(fan,spine))/dtor)
-stop
+
 warning = 0
 
 if (abs(90-acos(dot(fan,spine))/dtor) .lt. 10.) then
