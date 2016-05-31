1ee6ae2c5238297eb3de46ea0b84ea9bb00cafbb
Benjamin Williams <bmw@st-andrews.ac.uk>
2016-05-31 18:02:47 +0100

    Alternative null positions and removing the ghost cells

--- ssf.f90
+++ ssf.f90
@@ -57,57 +57,69 @@ program ssfind
   !open (unit=10,file='output/2null-edge.dat',form='unformatted')
   open (unit=10,file=filename,access='stream')
 
-    read(10),nx,ny,nz !number of vertices
+    read(10), nx, ny, nz !number of vertices
     print*, nx, ny, nz
     
-    allocate(bgrid(0:nx+1,0:ny+1,0:nz+1,3))
-    bgrid = 0
-    allocate(x(0:nx+1), y(0:ny+1), z(0:nz+1))
-    read(10),bgrid(1:nx,1:ny,1:nz,1)
-    read(10),bgrid(1:nx,1:ny,1:nz,2)
-    read(10),bgrid(1:nx,1:ny,1:nz,3)
-    read(10) x(1:nx), y(1:ny), z(1:nz)
-    
-    !copy the grid over for periodicity of coordinate systems
-    x(0) = 2*x(1) - x(2) !x,R,r don't repeat
-    x(nx+1) = 2*x(nx)-x(nx-1)
-    if (coord_type == 3) then !cylindrical
-      !phi repeating
-      bgrid(:,0,:,:) = bgrid(:,ny,:,:)
-      bgrid(:,ny+1,:,:) = bgrid(:,1,:,:)
-      y(0) = y(1) - (y(ny) - y(ny-1))
-      y(ny+1) = y(ny) + (y(1) - y(0))
-      z(0) = 2*z(1) - z(2)
-      z(nz+1) = 2*z(nz)-z(nz-1)
-    else if (coord_type == 2) then !spherical
-      !phi repeating
-      bgrid(:,:,0,:) = bgrid(:,:,nz,:)
-      bgrid(:,:,nz+1,:) = bgrid(:,:,1,:)
-      !have theta "periodic"
-      if (2*(nz/2) == nz) then !if nz (nphi) is even
-        !Top boundary
-        bgrid(:,0,1:nz/2,:) = bgrid(:,2,nz/2+1:nz,:)
-        bgrid(:,0,nz/2+1:nz,:) = bgrid(:,2,0:nz/2,:)
-        !Bottom boundary
-        bgrid(:,ny+1,1:nz/2,:) = bgrid(:,ny-1,nz/2+1:nz,:)
-        bgrid(:,ny+1,nz/2+1:nz,:) = bgrid(:,ny-1,0:nz/2,:)
-      else !if nz is odd
-        !Top boundary
-        bgrid(:,0,1:nz/2,:) = (bgrid(:,2,1+nz/2:nz-1,:) + bgrid(:,2,2+nz/2:nz,:))/2
-        bgrid(:,0,nz/2+1,:) = (bgrid(:,2,nz,:) + bgrid(:,2,1,:))/2
-        bgrid(:,0,nz/2+2:nz,:) = (bgrid(:,2,1:nz/2,:) + bgrid(:,2,2:nz/2+1,:))/2
-        !Bottom boundary
-        bgrid(:,ny+1,1:nz/2,:) = (bgrid(:,ny-1,1+nz/2:nz-1,:) + bgrid(:,ny-1,2+nz/2:nz,:))/2
-        bgrid(:,ny+1,nz/2+1,:) = (bgrid(:,ny-1,nz,:) + bgrid(:,ny-1,1,:))/2
-        bgrid(:,ny+1,nz/2+2:nz,:) = (bgrid(:,ny-1,1:nz/2,:) + bgrid(:,ny-1,2:nz/2+1,:))/2
+    if (1 == 0) then
+      allocate(bgrid(0:nx+1,0:ny+1,0:nz+1,3))
+      bgrid = 0
+      allocate(x(0:nx+1), y(0:ny+1), z(0:nz+1))
+      read(10), bgrid(1:nx,1:ny,1:nz,1)
+      read(10), bgrid(1:nx,1:ny,1:nz,2)
+      read(10), bgrid(1:nx,1:ny,1:nz,3)
+      read(10) x(1:nx), y(1:ny), z(1:nz)
+
+      !bgrid = bgrid(:,ny+1:0:-1,:,:)
+      !y = y(ny+1:0:-1)
+      
+      !copy the grid over for periodicity of coordinate systems
+      x(0) = 2*x(1) - x(2) !x,R,r don't repeat
+      x(nx+1) = 2*x(nx)-x(nx-1)
+      if (coord_type == 3) then !cylindrical
+        !phi repeating
+        bgrid(:,0,:,:) = bgrid(:,ny,:,:)
+        bgrid(:,ny+1,:,:) = bgrid(:,1,:,:)
+        y(0) = y(1) - (y(ny) - y(ny-1))
+        y(ny+1) = y(ny) + (y(1) - y(0))
+        z(0) = 2*z(1) - z(2)
+        z(nz+1) = 2*z(nz)-z(nz-1)
+      else if (coord_type == 2) then !spherical
+        !phi repeating
+        bgrid(:,:,0,:) = bgrid(:,:,nz,:)
+        bgrid(:,:,nz+1,:) = bgrid(:,:,1,:)
+        !have theta "periodic"
+        if (2*(nz/2) == nz) then !if nz (nphi) is even
+          !Top boundary
+          bgrid(:,0,1:nz/2,:) = bgrid(:,2,nz/2+1:nz,:)
+          bgrid(:,0,nz/2+1:nz,:) = bgrid(:,2,0:nz/2,:)
+          !Bottom boundary
+          bgrid(:,ny+1,1:nz/2,:) = bgrid(:,ny-1,nz/2+1:nz,:)
+          bgrid(:,ny+1,nz/2+1:nz,:) = bgrid(:,ny-1,0:nz/2,:)
+        else !if nz is odd
+          !Top boundary
+          bgrid(:,0,1:nz/2,:) = (bgrid(:,2,1+nz/2:nz-1,:) + bgrid(:,2,2+nz/2:nz,:))/2
+          bgrid(:,0,nz/2+1,:) = (bgrid(:,2,nz,:) + bgrid(:,2,1,:))/2
+          bgrid(:,0,nz/2+2:nz,:) = (bgrid(:,2,1:nz/2,:) + bgrid(:,2,2:nz/2+1,:))/2
+          !Bottom boundary
+          bgrid(:,ny+1,1:nz/2,:) = (bgrid(:,ny-1,1+nz/2:nz-1,:) + bgrid(:,ny-1,2+nz/2:nz,:))/2
+          bgrid(:,ny+1,nz/2+1,:) = (bgrid(:,ny-1,nz,:) + bgrid(:,ny-1,1,:))/2
+          bgrid(:,ny+1,nz/2+2:nz,:) = (bgrid(:,ny-1,1:nz/2,:) + bgrid(:,ny-1,2:nz/2+1,:))/2
+        endif
+        z(0) = 2*z(1) - z(2)
+        z(nz+1) = 2*z(nz)-z(nz-1)
+        y(0) = y(2)
+        y(ny+1) = y(ny-1)
+      else if (coord_type == 1) then
+        z(0) = 2*z(1) - z(2)
+        z(nz+1) = 2*z(nz)-z(nz-1)
       endif
-      z(0) = 2*z(1) - z(2)
-      z(nz+1) = 2*z(nz)-z(nz-1)
-      y(0) = y(2)
-      y(ny+1) = y(ny-1)
-    else if (coord_type == 1) then
-      z(0) = 2*z(1) - z(2)
-      z(nz+1) = 2*z(nz)-z(nz-1)
+    else
+      allocate(bgrid(nx,ny,nz,3))
+      allocate(x(nx), y(ny), z(nz))
+      read(10), bgrid(:,:,:,1)
+      read(10), bgrid(:,:,:,2)
+      read(10), bgrid(:,:,:,3)
+      read(10) x, y, z
     endif
 
     xmin = 1
@@ -135,25 +147,25 @@ program ssfind
   rnullsalt = rnulls
   if (coord_type == 2) then
     do i = 1, nnulls
-      if (rnullalt(2,i) < ymin+1)
-        rnullalt(2,i) = 2*zmin - rnullalt(2,i)
+      if (rnullsalt(2,i) < ymin+1) then
+        rnullsalt(2,i) = 2*zmin - rnullsalt(2,i)
         if (2*(nz/2) == nz) then
-          rnullalt = mod(rnullalt + nz/2 + 1,nz)
+          rnullsalt(3,i) = mod(rnullsalt(3,i) + nz/2 + 1, dble(nz))
         else
-          rnullalt = mod(rnullalt + nz/2 + 0.5 + 1, nz)
+          rnullsalt(3,i) = mod(rnullsalt(3,i) + nz/2 + 0.5 + 1, dble(nz))
         endif
-      elseif (rnullalt(2,i) > ymax-1)
-        runullalt(2,i) = 2*zmax - rnullalt(2,i)
+      elseif (rnullsalt(2,i) > ymax-1) then
+        rnullsalt(2,i) = 2*zmax - rnullsalt(2,i)
         if (2*(nz/2) == nz) then
-          rnullalt = mod(rnullalt + nz/2 + 1,nz)
+          rnullsalt(3,i) = mod(rnullsalt(3,i) + nz/2 + 1, dble(nz))
         else
-          rnullalt = mod(rnullalt + nz/2 + 0.5 + 1, nz)
+          rnullsalt(3,i) = mod(rnullsalt(3,i) + nz/2 + 0.5 + 1, dble(nz))
         endif
       endif
-      if (rnullalt(3,i) < zmax+1) then
-        rnullalt(3,i) = rnullalt(3,i) + zmax - 1
-      elseif (rnullalt(3,i) > zmax-1) then
-        rnullalt(3,i) = rnullalt(3,i) - zmax + 2
+      if (rnullsalt(3,i) < zmax+1) then
+        rnullsalt(3,i) = rnullsalt(3,i) + zmax - 1
+      elseif (rnullsalt(3,i) > zmax-1) then
+        rnullsalt(3,i) = rnullsalt(3,i) - zmax + 2
       endif
     enddo
   endif
@@ -161,7 +173,7 @@ program ssfind
   allocate(nsepss(nnulls))
 
   !signs=-1*signs
-  do nnull = 10,10!1, nnulls !loop over all nulls
+  do nnull = 45,45!1, nnulls !loop over all nulls
     print*, ''
     print*, 'Null number', nnull, 'of', nnulls
 
@@ -253,9 +265,9 @@ program ssfind
           !print*, r
 
           if (i < 50) then
-            h = 0.05
+            h = 5d-2
           else
-            h = 0.25
+            h = 25d-2
           endif
 
           call trace_line(r,1,sign,h) !trace line by a distance of h
@@ -304,7 +316,7 @@ program ssfind
           print*, 'Tracing has failed',i
           exitcondition = .true.
         endif
-        print*,'Checking at null', i, nlines, nnull
+        !print*,'Checking at null', i, nlines, nnull
         call at_null(nlines,nnull,i) !determine if point is at null
         call remove_points(nlines,i) !remove points from ring if necessary
         call add_points(nlines,i) !add points to ring if necessary
