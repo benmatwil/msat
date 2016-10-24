3296052eac0bfcbeb4455d6f14e00d494e65c60e
Benjamin Williams <bmw@st-andrews.ac.uk>
2016-10-24 18:58:12 +0100

    Added subroutine to get filenames related to earlier changes

--- params.f90
+++ params.f90
@@ -1,20 +1,21 @@
 module params
 
+  !implicit none
+
   ! basic parameters/constants
   double precision, parameter :: pi = 4.d0*atan(1.d0)
   double precision, parameter :: dtor = pi/180.d0
 
   ! data file containing the magnetic field data
-  character (len=100), parameter :: defaultfilename = 'data/magfield.dat'
-  character (len=100) :: filename, arg
-  integer :: icommand
+  character(100), parameter :: defaultfilename = 'magfield.dat'
+  character(100) :: filein, fileout, arg
 
   ! Number of threads to be used by OpenMP
   integer, parameter :: nproc = 2
 
   ! nullfinder parameters
   double precision, parameter :: zero = 1.d-10 !what the code treats as zero
-  integer, parameter :: sig_figs = 8 !the number of significant figures of accuracy required for the null
+  integer, parameter :: sig_figs = 6 !the number of significant figures of accuracy required for the null
 
   ! spinefinder parameters
   double precision, parameter :: rsphere = 1d-4 !radius of sphere which the dot products are calculated on
@@ -30,5 +31,27 @@ module params
   ! (2) = Spherical (r,theta,phi) (theta=polar angle, phi=azimuthal angle)
   ! (3) = Cylindrical (rho,phi,z)
   integer, parameter :: coord_type = 1
+
+  contains
+  
+    subroutine filenames
+
+      integer :: iarg, ichar
+
+      fileout = defaultfilename
+      if (command_argument_count() > 0) then
+        do iarg = 1, command_argument_count()
+          call get_command_argument(iarg,arg)
+          if (arg(1:5) == 'data=') then
+            fileout = trim(arg(6:))
+          endif
+        enddo
+      endif
+
+      filein = 'data/'//fileout
+      ichar = index(fileout, '.dat')
+      fileout = fileout(1:ichar-1)
+
+    end
 
 end module
