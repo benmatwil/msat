01cf381133bceb13245727f0a40ec1c7732b95de
Ben Williams <bmw@st-andrews.ac.uk>
2017-03-01 11:39:13 +0000

    Modified for new changes from make_cut

--- pyvis/plot_cut.py
+++ pyvis/plot_cut.py
@@ -11,11 +11,11 @@ def start(r, filename):
   datafile = filename
   prefile = rd.prefix(filename)
 
-  nulldata = rd.nulls(datafile, simple=True)
+  nulldata = rd.nulls(datafile)
 
   os.system(f'./make_cut -i {filename} -r {r}')
 
-  plt.figure(figsize=(10,5))
+  plt.figure(figsize=(16/2.55,9/2.55))
   plt.plot([])
   plt.xlabel('Longitude')
   plt.xlim([0,2*np.pi])
@@ -25,15 +25,21 @@ def start(r, filename):
   plt.yticks([0, np.pi/4, np.pi/2, np.pi*3/4, np.pi], [r'$0$', r'$\frac{\pi}{4}$', r'$\frac{\pi}{2}$', r'$\frac{3\pi}{4}$', r'$\pi$'])
   plt.tight_layout()
 
+#################################################################
+
 def spines():
   global prefile
 
+  cols = {-1:'blue', 0:'green', 1:'red'}
+
   with open('output/'+prefile+'-cut_spines.dat', 'rb') as spinefile:
-    null = np.fromfile(spinefile, dtype=np.int32, count=1)
-    while null > 0:
+    inull = np.fromfile(spinefile, dtype=np.int32, count=1)
+    while inull > 0:
       spine = np.fromfile(spinefile, dtype=np.float64, count=3)
-      plt.plot(spine[2], spine[1], '.', c='orange')
-      null = np.fromfile(spinefile, dtype=np.int32, count=1)
+      plt.plot(spine[2], spine[1], '.', c=cols[np.asscalar(nulldata[inull-1].sign)])
+      inull = np.fromfile(spinefile, dtype=np.int32, count=1)
+
+#################################################################
 
 def separators():
   global prefile
@@ -42,44 +48,47 @@ def separators():
     null = np.fromfile(sepfile, dtype=np.int32, count=1)
     while null > 0:
       sep = np.fromfile(sepfile, dtype=np.float64, count=3)
-      plt.plot(sep[2], sep[1], '.', c='red')
+      plt.plot(sep[2], sep[1], '.', c='purple')
       null = np.fromfile(sepfile, dtype=np.int32, count=1)
 
-def ringssort():
-  global prefile, nulldata
-
-  with open('output/'+prefile+'-cut_rings.dat', 'rb') as ringfile:
-    for i in range(nulldata.number.max()):
-      length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
-      ring = np.fromfile(ringfile, dtype=np.float64, count=3*length).reshape(-1,3)
-      if length > 0:
-        ring2 = ring.copy()
-        ring2[0,:] = ring2[-1,:]
-        ring2[1:,0] = 0
-        ring = ring[:-1,:]
-        for iring in range(ring2.shape[0]-1):
-          ptlist = set(range(ring.shape[0]))
-          dists = np.zeros(ring.shape[0], dtype=np.float64)
-          for ii, jring in enumerate(ptlist):
-            dists[ii] = np.sum((ring2[iring, :] - ring[jring, :])**2)
-          ring2[iring+1] = ring[dists.argmin(), :]
-          ptlist.remove(dists.argmin())
-          ring = ring[list(ptlist),:]
-        plt.plot(ring2[:,2], ring2[:,1])
+#################################################################
 
 def rings():
   global prefile, nulldata
 
   with open('output/'+prefile+'-cut_rings.dat', 'rb') as ringfile:
-    for i in range(nulldata.number.max()):
-      length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
+    length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
+    while length >= 0:
       ring = np.fromfile(ringfile, dtype=np.float64, count=3*length).reshape(-1,3)
       plt.plot(ring[:,2], ring[:,1])
+      # plt.plot(ring[:,2], ring[:,1], '.', c='blue')
+      length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
+
+#################################################################
+
+def hcs():
+  global prefile
+
+  with open('output/'+prefile+'-cut_hcs.dat', 'rb') as hcsfile:
+    length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
+    while length >= 0:
+      line = np.fromfile(hcsfile, dtype=np.float64, count=3*length).reshape(-1,3)
+      # print(line)
+      plt.plot(line[:,2], line[:,1], '.')
+      length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
+
+#################################################################
 
 def nulls():
   global datafile, nulldata
 
-  plt.plot(nulldata.pos[:,2], nulldata.pos[:,1], '.', c='green')
+  cols = {-1:'blue', 0:'green', 1:'red'}
+
+  for i in range(nulldata.shape[0]):
+    col = cols[nulldata[i-1].sign]
+    plt.plot(nulldata.pos[i,2], nulldata.pos[i,1], 'x', color=col)
+
+#################################################################
 
 def field():
   global datafile
