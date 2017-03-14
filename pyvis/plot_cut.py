ad25d638d60951d48f8b693885f420311b21a6cb
Ben Williams <bmw@st-andrews.ac.uk>
2017-03-14 18:09:43 +0000

    Many updates for new data structures etc.

--- pyvis/plot_cut.py
+++ pyvis/plot_cut.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyvis.read_data as rd
 import os
+from random import shuffle
 
 plt.ion()
 
@@ -23,7 +24,20 @@ def start(r, filename):
   plt.ylabel('Latitude')
   plt.ylim([np.pi,0])
   plt.yticks([0, np.pi/4, np.pi/2, np.pi*3/4, np.pi], [r'$0$', r'$\frac{\pi}{4}$', r'$\frac{\pi}{2}$', r'$\frac{3\pi}{4}$', r'$\pi$'])
-  plt.axhline(y=np.pi/2, ls='--')
+
+  br, _, _, rads, thetas, phis = rd.field(datafile)
+  levels = np.linspace(-10,10,201)
+  # print(np.where(r < rads)[0])
+  ir = np.where(r >= rads)[0].max()
+  # print(ir)
+  plotfield = br[ir, :, :] + (r - rads[ir])/(rads[ir+1] - rads[ir])*(br[ir+1, :, :] - br[ir, :, :])
+  plt.contourf(phis, thetas, plotfield, levels, cmap=plt.cm.Greys_r, extend='both', vmax=10, vmin=-10)
+  cb = plt.colorbar(fraction=0.05, pad=0.025)
+  cb.set_ticks([-10,-5,0,5,10])
+  cb.set_label('Magnetic Field Strength (G)')
+  plt.tight_layout()
+
+  # plt.axhline(y=np.pi/2, ls='--')
 
   # _, _, _, _, thetas, phis = rd.field(filename)
   # for phi in phis:
@@ -56,43 +70,59 @@ def separators():
     null = np.fromfile(sepfile, dtype=np.int32, count=1)
     while null > 0:
       sep = np.fromfile(sepfile, dtype=np.float64, count=3)
-      plt.plot(sep[2], sep[1], '*', c='purple')
+      plt.plot(sep[2], sep[1], '*', c='yellow')
       null = np.fromfile(sepfile, dtype=np.int32, count=1)
 
   with open('output/'+prefile+'-cut_seps_hcs.dat', 'rb') as sepfile:
     null = np.fromfile(sepfile, dtype=np.int32, count=1)
-    while null == 0:
-      print(null)
+    while null > 0:
       sep = np.fromfile(sepfile, dtype=np.float64, count=3)
-      plt.plot(sep[2], sep[1], '*', c='purple')
+      plt.plot(sep[2], sep[1], '*', c='orange')
       null = np.fromfile(sepfile, dtype=np.int32, count=1)
 
 #################################################################
 
-def rings():
+def rings(dots=False):
   global prefile, nulldata
 
+  negcolors, poscolors = plt.get_cmap('Blues'), plt.get_cmap('Reds')
+  negcolors = [negcolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == 1))]
+  poscolors = [poscolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == -1))]
+  shuffle(poscolors)
+  shuffle(negcolors)
+  colors, ipos, ineg = [], 0, 0
+  for inull in range(nulldata.number[-1]):
+    if nulldata[inull].sign == -1:
+      col = negcolors[ipos]
+      ipos += 1
+    elif nulldata[inull].sign == 1:
+      col = poscolors[ineg]
+      ineg += 1
+    colors = colors + [col]
+
   with open('output/'+prefile+'-cut_rings.dat', 'rb') as ringfile:
-    length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
-    while length >= 0:
-      ring = np.fromfile(ringfile, dtype=np.float64, count=3*length).reshape(-1,3)
-      plt.plot(ring[:,2], ring[:,1])
-      plt.plot(ring[:,2], ring[:,1], '.', c='green')
+    inull = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
+    while inull >= 0:
       length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
+      ring = np.fromfile(ringfile, dtype=np.float64, count=3*length).reshape(-1,3)
+      plt.plot(ring[:,2], ring[:,1], c=colors[inull-1])
+      if dots == True:
+        plt.plot(ring[:,2], ring[:,1], '.', c='green')
+      inull = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
 
 #################################################################
 
-def hcs():
+def hcs(dots=False):
   global prefile
 
   with open('output/'+prefile+'-cut_hcs.dat', 'rb') as hcsfile:
     length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
-    print(length)
     while length >= 0:
       print(length)
       line = np.fromfile(hcsfile, dtype=np.float64, count=3*length).reshape(-1,3)
-      # print(line)
-      plt.plot(line[:,2], line[:,1], '--')
+      plt.plot(line[:,2], line[:,1], ls=':', c='lime')
+      if dots == True:
+        plt.plot(line[:,2], line[:,1], '.', c='blue')
       length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
 
 #################################################################
