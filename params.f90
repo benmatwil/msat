44abd924ebcb51b5b31619d3c0f3569876f44c8b
Benjamin Williams <bmw@st-andrews.ac.uk>
2017-03-23 19:51:03 +0000

    Rearranged and changed params to be more user friendly

--- params.f90
+++ params.f90
@@ -4,35 +4,34 @@ module params
 
   implicit none
 
-  ! basic parameters/constants
-  real(np), parameter :: pi = acos(-1.0_np)
-  real(np), parameter :: dtor = pi/180.0_np
-
   ! data file containing the magnetic field data
   character(100), parameter :: defaultfilename = 'data/magfield.dat'
   character(100), parameter :: outputdir = 'output'
-  character(100) :: filein, fileout
 
   ! Number of threads to be used by OpenMP
-  integer(int32), parameter :: nproc = 4
+  integer(int32), parameter :: nproc = 4  ! set to zero to use the environment variable OMP_NUM_THREADS
 
   ! nullfinder parameters
   real(np), parameter :: zero = 1e-10_np ! what the code treats as zero
   integer(int32), parameter :: sig_figs = 6 ! the number of significant figures of accuracy required for the null
 
   ! spinefinder parameters
-  real(np), parameter :: rsphere = 1e-4_np ! radius of sphere which the dot products are calculated on
-  integer(int32), parameter :: nphi = 90 ! number of points on sphere to start
-  integer(int32), parameter :: ntheta = nphi/2
+  real(np), parameter :: rspherefact = 50.0_np ! radius factor (in grid coordinates) of sphere which to place start points (rsphere = rspherefact*10**-sig_figs)
+  integer(int32), parameter :: nphi = 90 ! number of start points in phi direction
+  integer(int32), parameter :: ntheta = nphi/2 ! number of start points in theta direction
 
   ! ssfind parameters
-  integer(int32), parameter :: nstart = 100 ! number of startpoints in ring
-  integer(int32), parameter :: ringsmax = 10000 ! maximum # of rings
+  integer(int32), parameter :: nstart = 500 ! number of startpoints in ring
+  integer(int32), parameter :: ringsmax = 10000 ! maximum number of rings
   integer(int32), parameter :: pointsmax = 100000 ! maximum number of points in ring
-  real(np), parameter :: stepsize = 0.1_np ! 0.25_np ! step size h after 50 iterations (otherwise 5 times smaller)
+  real(np), parameter :: stepsize = 0.025_np ! 0.25_np ! step size h after 50 iterations (otherwise 5 times smaller)
   real(np), parameter :: tol = 1e-6_np ! 1e-5_np ! tolerance of rkf45 scheme
   real(np), parameter :: stepmin = 1e-5_np ! 3e-4_np ! minimum step length
-  integer(int32), parameter :: skiprings = 30 ! Rings to skip from total in final ring file
+
+  ! NO NEED TO CHANGE BEYOND HERE (basic parameters/constants)
+  real(np), parameter :: pi = acos(-1.0_np)
+  real(np), parameter :: dtor = pi/180.0_np
+  character(100) :: filein, fileout
 
   contains
   
