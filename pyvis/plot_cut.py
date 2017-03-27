541dbf7faffe7aee19488d25f99cbca947159b38
Benjamin Williams <bmw@st-andrews.ac.uk>
2017-03-27 19:20:55 +0100

    Many updates to plot cut

--- pyvis/plot_cut.py
+++ pyvis/plot_cut.py
@@ -6,17 +6,19 @@ from random import shuffle
 
 plt.ion()
 
-def start(r, filename):
-    global datafile, prefile, nulldata
+def start(r, filename, title=False):
+    global datafile, prefile, nulldata, rstr
 
     datafile = filename
     prefile = rd.prefix(filename)
 
     nulldata = rd.nulls(datafile)
 
-    os.system(f'./make_cut -i {filename} -r {r}')
+    rstr = '{:6.4f}'.format(r)
 
-    plt.figure(figsize=(16/2.55,9/2.55))
+    # os.system(f'./make_cut -i {filename} -r {r}')
+
+    plt.figure(figsize=(16/2.55,9/2.55)) # set-up for A4
     plt.plot([])
     plt.xlabel('Longitude')
     plt.xlim([0,2*np.pi])
@@ -26,16 +28,16 @@ def start(r, filename):
     plt.yticks([0, np.pi/4, np.pi/2, np.pi*3/4, np.pi], [r'$0$', r'$\frac{\pi}{4}$', r'$\frac{\pi}{2}$', r'$\frac{3\pi}{4}$', r'$\pi$'])
 
     br, _, _, rads, thetas, phis = rd.field(datafile)
-    levels = np.linspace(-10,10,201)
-    # print(np.where(r < rads)[0])
+    levels = np.linspace(-10,10,101)
     ir = np.where(r >= rads)[0].max()
-    # print(ir)
     plotfield = br[ir, :, :] + (r - rads[ir])/(rads[ir+1] - rads[ir])*(br[ir+1, :, :] - br[ir, :, :])
     plt.contourf(phis, thetas, plotfield, levels, cmap=plt.cm.Greys_r, extend='both', vmax=10, vmin=-10)
     cb = plt.colorbar(fraction=0.05, pad=0.025)
     cb.set_ticks([-10,-5,0,5,10])
     cb.set_label('Magnetic Field Strength (G)')
     plt.tight_layout()
+    
+    plt.title('Cut: ' + datafile + ' r = ' + rstr)
 
     # plt.axhline(y=np.pi/2, ls='--')
 
@@ -47,37 +49,39 @@ def start(r, filename):
 
     plt.tight_layout()
 
+def all(labels=False):
+    rings(labels=labels)
+    spines(labels=labels)
+    separators(labels=labels)
+    hcs()
+
 #################################################################
 
 def spines(labels=False):
-    global prefile
-
     cols = {-1:'blue', 0:'green', 1:'red'}
 
-    with io.open('output/'+prefile+'-cut_spines.dat', 'rb') as spinefile:
+    with io.open('output/'+prefile+'-spines-cut_'+rstr+'.dat', 'rb') as spinefile:
         inull = np.asscalar(np.fromfile(spinefile, dtype=np.int32, count=1))
         while inull > 0:
             spine = np.fromfile(spinefile, dtype=np.float64, count=3)
             plt.plot(spine[2], spine[1], '.', c=cols[np.asscalar(nulldata[inull-1].sign)])
             if labels == True:
-                plt.text(spine[2], spine[1], f'{inull}', color='green')
+                plt.text(spine[2], spine[1], '{}'.format(inull), color='green')
             inull = np.fromfile(spinefile, dtype=np.int32, count=1)
 
 #################################################################
 
 def separators(labels=False):
-    global prefile
-
-    with io.open('output/'+prefile+'-cut_seps.dat', 'rb') as sepfile:
+    with io.open('output/'+prefile+'-separators-cut_'+rstr+'.dat', 'rb') as sepfile:
         null = np.asscalar(np.fromfile(sepfile, dtype=np.int32, count=1))
         while null > 0:
             sep = np.fromfile(sepfile, dtype=np.float64, count=3)
             plt.plot(sep[2], sep[1], '*', c='yellow')
             if labels == True:
-                plt.text(sep[2], sep[1], f'{null}')
+                plt.text(sep[2], sep[1], '{}'.format(null))
             null = np.asscalar(np.fromfile(sepfile, dtype=np.int32, count=1))
 
-    with io.open('output/'+prefile+'-cut_seps_hcs.dat', 'rb') as sepfile:
+    with io.open('output/'+prefile+'-hcs-separators-cut_'+rstr+'.dat', 'rb') as sepfile:
         null = np.asscalar(np.fromfile(sepfile, dtype=np.int32, count=1))
         while null > 0:
             sep = np.fromfile(sepfile, dtype=np.float64, count=3)
@@ -87,8 +91,6 @@ def separators(labels=False):
 #################################################################
 
 def rings(dots=False, labels=False):
-    global prefile, nulldata
-
     negcolors, poscolors = plt.get_cmap('Blues'), plt.get_cmap('Reds')
     negcolors = [negcolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == -1))]
     poscolors = [poscolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == 1))]
@@ -104,14 +106,14 @@ def rings(dots=False, labels=False):
             ipos += 1
         colors = colors + [col]
 
-    with io.open('output/'+prefile+'-cut_rings.dat', 'rb') as ringfile:
+    with io.open('output/'+prefile+'-rings-cut_'+rstr+'.dat', 'rb') as ringfile:
         inull = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
         while inull >= 0:
             length = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
             ring = np.fromfile(ringfile, dtype=np.float64, count=3*length).reshape(-1,3)
             plt.plot(ring[:,2], ring[:,1], c=colors[inull-1])
             if labels == True:
-                plt.text(ring[length//2,2], ring[length//2,1], f'{inull}')
+                plt.text(ring[length//2,2], ring[length//2,1], '{}'.format(inull))
             if dots == True:
                 plt.plot(ring[:,2], ring[:,1], '.', c='green')
             inull = np.asscalar(np.fromfile(ringfile, dtype=np.int32, count=1))
@@ -119,36 +121,31 @@ def rings(dots=False, labels=False):
 #################################################################
 
 def hcs(dots=False):
-    global prefile
-
-    with io.open('output/'+prefile+'-cut_hcs.dat', 'rb') as hcsfile:
-        length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
-        while length >= 0:
-            print(length)
+    with io.open('output/'+prefile+'-hcs-cut_'+rstr+'.dat', 'rb') as hcsfile:
+        ihcs = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
+        while ihcs >= 0:
+            length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
+            # print(length)
             line = np.fromfile(hcsfile, dtype=np.float64, count=3*length).reshape(-1,3)
             plt.plot(line[:,2], line[:,1], ls=':', c='lime')
             if dots == True:
                 plt.plot(line[:,2], line[:,1], '.', c='blue')
-            length = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
+            ihcs = np.asscalar(np.fromfile(hcsfile, dtype=np.int32, count=1))
 
 #################################################################
 
 def nulls(labels=False):
-    global datafile, nulldata
-
     cols = {-1:'blue', 0:'green', 1:'red'}
 
     for i in range(nulldata.shape[0]):
         col = cols[nulldata[i].sign]
         plt.plot(nulldata.pos[i,2], nulldata.pos[i,1], 'x', color=col)
         if labels == True:
-            plt.text(nulldata.pos[i,2], nulldata.pos[i,1], f'{i}, {nulldata.pos[i,0]:04f}')
+            plt.text(nulldata.pos[i,2], nulldata.pos[i,1], '{}, {:04f}'.format(i, nulldata.pos[i,0]))
 
 #################################################################
 
 def field():
-    global datafile
-
     br, _, _, rads, thetas, phis = rd.field(datafile)
     levels = np.linspace(-10,10,101)
     plt.contourf(phis, thetas, br[0,:,:], levels, cmap=plt.cm.RdBu_r, extend='both', vmax=10, vmin=-10)
@@ -156,3 +153,8 @@ def field():
     cb.set_ticks([-10,-5,0,5,10])
     cb.set_label('Magnetic Field Strength (G)')
     plt.tight_layout()
+
+#################################################################
+def save(type, dpi=600):
+    savename = 'figures/'+prefile+'-cut_'+rstr+'.{}'.format(type)
+    plt.savefig(savename, dpi=dpi)
