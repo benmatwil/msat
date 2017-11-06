422cf62e3fe70c327e26ed1f050b4fdfec6c61e5
Ben Williams <bmw@st-andrews.ac.uk>
2017-11-06 10:07:15 +0000

    Some minor updates to read

--- pyvis/read.py
+++ pyvis/read.py
@@ -11,11 +11,13 @@ def synmap(filename):
     with open(filename, 'rb') as fieldfile:
         nlon = np.asscalar(np.fromfile(fieldfile, count=1, dtype=np.int32))
         nlat = np.asscalar(np.fromfile(fieldfile, count=1, dtype=np.int32))
-        bsyn = np.fromfile(fieldfile, count=nlon*nlat, dtype=np.float64).reshape(nlat,nlon)
+
+        bsyn = np.fromfile(fieldfile, count=nlon*nlat, dtype=np.float64).reshape((nlon, nlat), order='f')
+
         lons = np.fromfile(fieldfile, count=nlon, dtype=np.float64)
         lats = np.fromfile(fieldfile, count=nlat, dtype=np.float64)
 
-        return bsyn, lons, lats
+    return bsyn, lons, lats
 
 def field(filename):
 
@@ -23,9 +25,13 @@ def field(filename):
         nrad = np.asscalar(np.fromfile(fieldfile, count=1, dtype=np.int32))
         ntheta = np.asscalar(np.fromfile(fieldfile, count=1, dtype=np.int32))
         nphi = np.asscalar(np.fromfile(fieldfile, count=1, dtype=np.int32))
-        br = np.fromfile(fieldfile, count=nrad*ntheta*nphi, dtype=np.float64).reshape(nphi,ntheta,nrad).T
-        bt = np.fromfile(fieldfile, count=nrad*ntheta*nphi, dtype=np.float64).reshape(nphi,ntheta,nrad).T
-        bp = np.fromfile(fieldfile, count=nrad*ntheta*nphi, dtype=np.float64).reshape(nphi,ntheta,nrad).T
+
+        shape = (nrad, ntheta, nphi)
+        num = nrad*ntheta*nphi
+        br = np.fromfile(fieldfile, count=num, dtype=np.float64).reshape(shape, order='f')
+        bt = np.fromfile(fieldfile, count=num, dtype=np.float64).reshape(shape, order='f')
+        bp = np.fromfile(fieldfile, count=num, dtype=np.float64).reshape(shape, order='f')
+
         rads = np.fromfile(fieldfile, count=nrad, dtype=np.float64)
         thetas = np.fromfile(fieldfile, count=ntheta, dtype=np.float64)
         phis = np.fromfile(fieldfile, count=nphi, dtype=np.float64)
@@ -36,20 +42,20 @@ def nulls(filename, simple=False):
 
     with open('output/'+prefix(filename)+'-nullpos.dat', 'rb') as nullfile:
         nnulls = np.asscalar(np.fromfile(nullfile, dtype=np.int32, count=1))
-        gridpos = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls,3)
-        pos = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls,3)
+        gridpos = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls, 3)
+        pos = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls, 3)
 
     if simple == False:
         with open('output/'+prefix(filename)+'-nulldata.dat', 'rb') as nullfile:
             nnulls = np.asscalar(np.fromfile(nullfile, dtype=np.int32, count=1))
             signs = np.fromfile(nullfile, dtype=np.int32, count=nnulls)
-            spines = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls,3)
-            fans = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls,3)
+            spines = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls, 3)
+            fans = np.fromfile(nullfile, dtype=np.float64, count=3*nnulls).reshape(nnulls, 3)
             warning = np.fromfile(nullfile, dtype=np.int32, count=nnulls)
 
         nulls = np.recarray(nnulls, dtype=[('number',np.int32),
-            ('pos',np.float64,3),('gridpos',np.float64,3),
-            ('sign',np.int32),('spine',np.float64,3),('fan',np.float64,3),('warning',np.int32)])
+            ('pos',np.float64,3), ('gridpos',np.float64,3),
+            ('sign',np.int32), ('spine',np.float64,3), ('fan',np.float64,3), ('warning',np.int32)])
 
         nulls.sign = signs
         nulls.spine = spines
@@ -57,7 +63,7 @@ def nulls(filename, simple=False):
         nulls.warning = warning
     else:
         nulls = np.recarray(nnulls, dtype=[('number',np.int32),
-            ('pos',np.float64,3),('gridpos',np.float64,3)])
+            ('pos',np.float64,3), ('gridpos',np.float64,3)])
 
     nulls.number = np.arange(nnulls, dtype=np.int32)+1
     nulls.pos = pos
@@ -82,6 +88,17 @@ def separators(filename, lines=True, connectivity=True, hcs=False):
     coni = []
     sepi = []
 
+    # with open(connectivityfile, 'rb') as sepinfo:
+    #     start = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
+    #     while start > 0:
+    #         end = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
+    #         sepinfo.seek(8, 1)
+    #         coni.append(end)
+    #         start = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
+    #         if start == 
+
+
+
     with open(connectivityfile, 'rb') as sepinfo:
         with open(separatorsfile, 'rb') as seps:
             start = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
@@ -91,7 +108,7 @@ def separators(filename, lines=True, connectivity=True, hcs=False):
                     end = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
                     sepinfo.seek(8, 1)
                     length = np.asscalar(np.fromfile(seps, dtype=np.int32, count=1))
-                    separator = np.fromfile(seps, dtype=np.float64, count=3*length).reshape(-1,3)
+                    separator = np.fromfile(seps, dtype=np.float64, count=3*length).reshape(-1, 3)
                     coni.append(end)
                     sepi.append(separator)
                     start = np.asscalar(np.fromfile(sepinfo, dtype=np.int32, count=1))
@@ -124,9 +141,11 @@ def separators(filename, lines=True, connectivity=True, hcs=False):
     else:
         return seplist, conlist
 
-def spines(filename):
+def spines(filename, nulls=None):
 
     nulldata = nulls(filename, simple=True)
+    if nulls is None:
+        nulls = nulldata.number
 
     spinelist = []
 
@@ -135,7 +154,10 @@ def spines(filename):
             spinelisti = []
             for ispine in range(2):
                 length = np.asscalar(np.fromfile(spinefile, dtype=np.int32, count=1))
-                spinelisti.append(np.fromfile(spinefile, dtype=np.float64, count=3*length).reshape(-1,3))
+                if inull in nulls:
+                    spinelisti.append(np.fromfile(spinefile, dtype=np.float64, count=3*length).reshape(-1, 3))
+                else:
+                    spinefile.seek(length*3*8, 1)
             spinelist.append(spinelisti)
 
     return spinelist
@@ -160,7 +182,7 @@ def rings(filename, allinfo=False, nskip=1):
                 while iring < ringsmax and lengths[iring] > 0:
                     assoclisti.append(np.fromfile(ringfile, dtype=np.int32, count=lengths[iring]))
                     breaklisti.append(np.fromfile(ringfile, dtype=np.int32, count=lengths[iring]))
-                    ringlisti.append(np.fromfile(ringfile, dtype=np.float64, count=3*lengths[iring]).reshape(-1,3))
+                    ringlisti.append(np.fromfile(ringfile, dtype=np.float64, count=3*lengths[iring]).reshape(-1, 3))
                     iskip = 1
                     while iskip + iring < ringsmax and iskip < nskip:
                         ringfile.seek(lengths[iring+iskip]*32, 1)
