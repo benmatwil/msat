0e4288155020a70d12d73288339df99c5e2f2f96
Benjamin Williams <bmw@st-andrews.ac.uk>
2017-11-25 17:42:15 +0000

    Changed default h for fieldline tracing for new routine

--- pyvis/nullstruct.py
+++ pyvis/nullstruct.py
@@ -114,7 +114,7 @@ def plot(n, filename, converge=True, fan=False, ball=True, rsphere=1e-4, h0=3e-2
         countf = 0
         countb = 0
         for startpt in startpts:
-            h = h0*10
+            h = h0
             line = fl.fieldline3d(startpt + nullpos, bgrid, xgc,ygc,zgc, h, 0.1*h, 10*h, 0.01*h, boxedge=boxedge, oneway=True, gridcoord=True)
             plt.plot(line[:,0],line[:,1],line[:,2],c='red')
             if (line[-1,0] < boxedge[0,0] + rsphere*1e-3 or line[-1,0] > boxedge[1,0] - rsphere*1e-3 or
