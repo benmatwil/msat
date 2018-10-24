ec28bc86b3268d757c1b73706b43989ef6167384
Benjamin Williams <bmw@st-andrews.ac.uk>
2018-10-24 11:17:14 +0100

    Extra commenting throughout plot_cut

--- pyvis/plot_cut.py
+++ pyvis/plot_cut.py
@@ -1,3 +1,6 @@
+# example file to plot radial cuts through global magnetic field models of the solar corona
+# can produce the majority of plots found in the Williams (2018)
+
 import numpy as np
 import matplotlib.pyplot as plt
 from . import read as rd
@@ -7,9 +10,10 @@ from fractions import Fraction as fr
 
 plt.ion()
 
-outprefix = 'output'
-
 def get_colours():
+    """
+    Pick random colours for each null point in shades of red and blue
+    """
     negcolors, poscolors = plt.get_cmap('Blues'), plt.get_cmap('Reds')
     negcolors = [negcolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == -1))]
     poscolors = [poscolors(i) for i in np.linspace(0.5, 1, np.count_nonzero(nulldata.sign == 1))]
@@ -29,7 +33,11 @@ def get_colours():
 
 #################################################################
 
-def ticks(nsplit, npi):
+def latex_ticks(nsplit, npi):
+    """
+    Produces strings for the ticks used for some spherical coordinate plots in
+    latex for multiples of pi
+    """
     list = []
     for i in range(nsplit+1):
         string = r'$'
@@ -51,16 +59,18 @@ def ticks(nsplit, npi):
 #################################################################
 
 def start(filename, norm, d, title=False, levels=np.linspace(-20,20,101), colmap=plt.cm.Greys_r, nticks=4, newcolours=True, linerast=False, output_dir=None):
+    """
+    Function to create the plot and prepare everything plotting using other functions
+    """
     global datafile, prefile, nulldata, plane_norm, plane_d, colours
 
     datafile = filename
     prefile = rd.prefix(filename)
 
     if output_dir is not None:
-        outprefix = output_dir
-        rd.outprefix = outprefix
+        rd.outprefix = output_dir
 
-    nulldata = rd.nulls(datafile)
+    nulldata = rd.nulls(datafile, simple=True)
 
     plane_norm = norm
     try:
@@ -77,10 +87,10 @@ def start(filename, norm, d, title=False, levels=np.linspace(-20,20,101), colmap
     ax = plt.gca()
     plt.xlabel(r'Longitude, \(\phi\)')
     plt.xlim([0, 2*np.pi])
-    plt.xticks(np.linspace(0, 2*np.pi, nticks+1), ticks(nticks, 2))
+    plt.xticks(np.linspace(0, 2*np.pi, nticks+1), latex_ticks(nticks, 2))
     plt.ylabel(r'Colatitude, \(\theta\)')
     plt.ylim([np.pi, 0])
-    plt.yticks(np.linspace(0, np.pi, nticks+1), ticks(nticks, 1))
+    plt.yticks(np.linspace(0, np.pi, nticks+1), latex_ticks(nticks, 1))
 
     if linerast == False:
         ax.set_rasterization_zorder(-6)
@@ -96,13 +106,16 @@ def start(filename, norm, d, title=False, levels=np.linspace(-20,20,101), colmap
     cb.set_ticks(np.linspace(0, 1.0, 5)*diff + levels[0])
     cb.set_label('Radial Field Strength (G)')
     plt.tight_layout()
-    if title == True: plt.title('Cut: ' + datafile + ' r = ' + rstr)
+    if title: plt.title('Cut: ' + datafile + ' r = ' + rstr)
 
-    if newcolours == True: colours = get_colours()
+    if newcolours: colours = get_colours()
 
 #################################################################
 
 def all(labels=False, ms=3):
+    """
+    Just calls all other functions for convenience
+    """
     lw = 1
     rings(labels=labels, lw=lw)
     hcs(lw=lw)
@@ -113,6 +126,9 @@ def all(labels=False, ms=3):
 #################################################################
 
 def spines(labels=False, ms=3):
+    """
+    Plots all the spine points
+    """
     null_nums, spines = rd.cut_spines(datafile, plane_norm, plane_d)
     for inull, spine in zip(null_nums, spines):
         plt.plot(spine[2], spine[1], '.', c=colours[inull-1], ms=ms, zorder=-5)
@@ -122,6 +138,9 @@ def spines(labels=False, ms=3):
 #################################################################
 
 def separators(labels=False, ms=3, hcs=False):
+    """
+    Plots all the separator points
+    """
     null_nums, separators = rd.cut_separators(datafile, plane_norm, plane_d, hcs=hcs)
     col = 'orange' if hcs else 'yellow'
     for inull, sep in zip(null_nums, separators):
@@ -132,6 +151,9 @@ def separators(labels=False, ms=3, hcs=False):
 #################################################################
 
 def rings(dots=False, labels=False, lw=1):
+    """
+    Plots all the lines denoting the ring crossings
+    """
     null_nums, rings = rd.cut_sepsurf(datafile, plane_norm, plane_d)
     for inull, ring in zip(null_nums, rings):
         plt.plot(ring[:, 2], ring[:, 1], c=colours[inull-1], lw=lw, zorder=-5)
@@ -143,6 +165,9 @@ def rings(dots=False, labels=False, lw=1):
 #################################################################
 
 def hcs(dots=False, lw=1):
+    """
+    Plots all the hcs crossings
+    """
     lines = rd.cut_hcs(datafile, plane_norm, plane_d)
     for line in lines:
         plt.plot(line[:, 2], line[:, 1], c='lime', ls=':', lw=lw, zorder=-0.5)
@@ -164,6 +189,9 @@ def hcs(dots=False, lw=1):
 #################################################################
 
 def nulls(labels=False, size=1, zorder=-1):
+    """
+    Plots all the null point locations in theta/phi
+    """
     for i in range(nulldata.shape[0]):
         plt.plot(nulldata.pos[i, 2], nulldata.pos[i, 1], '.', color=colours[i], ms=size, zorder=zorder)
         if labels == True:
@@ -172,20 +200,13 @@ def nulls(labels=False, size=1, zorder=-1):
 #################################################################
 
 def field():
+    """
+    Plots the background radial field on the solar surface
+    """
     br, _, _, rads, thetas, phis = rd.field(datafile)
-    levels = np.linspace(-10,10,101)
-    plt.contourf(phis, thetas, br[0,:,:], levels, cmap=plt.cm.RdBu_r, extend='both', vmax=10, vmin=-10, zorder=-10)
+    levels = np.linspace(-10, 10, 101)
+    plt.contourf(phis, thetas, br[0, :, :], levels, cmap=plt.cm.RdBu_r, extend='both', vmax=10, vmin=-10, zorder=-10)
     cb = plt.colorbar(fraction=0.05, pad=0.025)
-    cb.set_ticks([-10,-5,0,5,10])
+    cb.set_ticks([-10, -5, 0, 5, 10])
     cb.set_label('Magnetic Field Strength (G)')
     plt.tight_layout()
-
-#################################################################
-def save(type, dpi=1200, figdir='figures', extra=None):
-    if extra is not None:
-        addin = '-' + extra
-    else:
-        addin = ''
-    savename = figdir+'/'+prefile+'-cut_'+rstr+addin+'.{}'.format(type)
-    plt.savefig(savename, dpi=dpi)
-    print('Saved to {}'.format(savename))
