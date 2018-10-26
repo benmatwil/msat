80cf890206034706b6ef677929e5202c0e9f71da
Benjamin Williams <bmw@st-andrews.ac.uk>
2018-10-26 14:44:57 +0100

    Tidy up of nullstruct code -- maybe come back to deal with grids

--- pyvis/nullstruct.py
+++ pyvis/nullstruct.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import os
 
-def plot(n, filename, converge=True, fan=False, ball=True, rsphere=1e-4, h0=5e-2, badtest=False, coordsystem='cartesian'):
+def plot(n, filename, converge=True, fan=False, ball=True, rsphere=1e-4, h0=5e-2, coordsystem='cartesian', draw_scale=1, figsize=(10, 10)):
 
     if converge == True:
-        
         if coordsystem == 'cartesian':
             coord = 'xyz'
         elif coordsystem == 'cylindrical':
@@ -18,16 +17,7 @@ def plot(n, filename, converge=True, fan=False, ball=True, rsphere=1e-4, h0=5e-2
         os.system('make sf{} debug=on'.format(coord))
         os.system('./sf{0} -i {1} -n {2:04d}'.format(coord, filename, n) )
 
-    field = rd.field(filename)
-    bgrid = np.zeros((field[0].shape[0], field[0].shape[1], field[0].shape[2], 3), dtype=np.float64)
-    for i in range(3):
-        bgrid[:, :, :, i] = field[i]
-    
-    rads = field[3]
-    thetas = field[4]
-    phis = field[5]
-
-    del field
+    bgrid, xgc, ygc, zgc = rd.field(filename, grid=True)
     
     xgc = np.arange(bgrid.shape[0])
     ygc = np.arange(bgrid.shape[1])
@@ -39,127 +29,106 @@ def plot(n, filename, converge=True, fan=False, ball=True, rsphere=1e-4, h0=5e-2
     xsize = 20
     xthick = 5
 
-    plt.figure(figsize=(12, 12), tight_layout=True)
+    plt.figure(figsize=figsize, tight_layout=True)
     plt.axes(projection='3d')
-    plt.plot([nullpos[0]], [nullpos[1]], [nullpos[2]], 'rx ', ms=xsize, mew=xthick)
+    plt.plot(*nullpos[:, None], 'rx ', ms=xsize, mew=xthick)
 
     plt.axis('off')
 
-    if converge == True:
+    if converge:
         fnames = ['maxvec.dat', 'minvec.dat', 'spine.dat']
         col = ['black', 'green', 'red']
 
         for j, name in enumerate(fnames):
             with open('output/' + name, 'rb') as file:
                 ny = np.asscalar(np.fromfile(file, count=1, dtype=np.int32))
-                r = np.fromfile(file, count=3*ny, dtype=np.float64).reshape(ny,3).T
+                r = np.fromfile(file, count=3*ny, dtype=np.float64).reshape(ny, 3).T
                 r = r * rsphere
 
                 for i in range(3):
-                    r[i,:] = r[i,:] + nullpos[i]
+                    r[i, :] = r[i, :] + nullpos[i]
 
-                plt.plot(r[0, :], r[1, :], r[2, :], 'x ', c=col[j])
+                plt.plot(*r, 'x ', c=col[j])
 
                 if name == fnames[0]:
                     maxvec = np.fromfile(file, count=3, dtype=np.float64)
                     max1 = maxvec.copy()
                     maxvec = maxvec*rsphere
                     maxvec = maxvec + nullpos
-                    plt.plot([maxvec[0]], [maxvec[1]], [maxvec[2]],'x ', c='cyan', ms=xsize, mew=xthick)
+                    plt.plot(*maxvec[:, None],'x ', c='cyan', ms=xsize, mew=xthick)
 
                 if name == fnames[1]:
                     minvec = np.fromfile(file, count=3, dtype=np.float64)
                     min1 = minvec.copy()
                     minvec = minvec*rsphere
                     minvec = minvec + nullpos
-                    plt.plot([minvec[0]], [minvec[1]], [minvec[2]],'x ', c='green', ms=xsize, mew=xthick)
+                    plt.plot(*minvec[:, None],'x ', c='green', ms=xsize, mew=xthick)
 
                 if name == fnames[2]:
                     spine = np.fromfile(file, count=3, dtype=np.float64)
                     spine = spine*rsphere
                     spine = spine + nullpos
-                    plt.plot([spine[0]], [spine[1]], [spine[2]],'x ', c='blue', ms=xsize, mew=xthick)
+                    plt.plot(*spine[:, None],'x ', c='blue', ms=xsize, mew=xthick)
 
-        fanpts = []
         npts = 121
-        for iangle in range(npts):
-            angle = 2*np.pi*iangle/(npts-1)
-            fanpts = fanpts + [(max1*np.cos(angle) + min1*np.sin(angle))*rsphere + nullpos]
-        fanpts = np.array(fanpts)
-        plt.plot(fanpts[:,0], fanpts[:,1], fanpts[:,2], c='black')
+        angles = np.linspace(0, 2*np.pi, npts)
+        fanpts = np.array([(max1*np.cos(angle) + min1*np.sin(angle))*rsphere + nullpos for angle in angles])
+        plt.plot(*fanpts.T, c='black')
 
         spineline = np.array([spine, 2*nullpos - spine])
-        plt.plot(spineline[:,0], spineline[:,1], spineline[:,2], c='blue')
+        plt.plot(*spineline.T, c='blue')
     
-    boxedge = np.zeros((2,3), dtype=np.float64)
+    boxedge = np.zeros((2, 3), dtype=np.float64)
     for i in range(3):
-        boxedge[:,i] = np.array([nullpos[i] - rsphere, nullpos[i] + rsphere])
+        boxedge[:, i] = np.array([nullpos[i] - draw_scale*rsphere, nullpos[i] + draw_scale*rsphere])
+    boxedge = np.array([[nullpos[i] - draw_scale*rsphere, nullpos[i] + draw_scale*rsphere] for i in range(3)]).T
     
-    startpts = []
+    startpts_fan = []
     if fan == True:
         rad = 0.6*rsphere
         nphi = 20
-        phi = 2*np.pi*(np.arange(nphi)+0.5)/nphi
-        for i in range(nphi):
-            startpts = startpts + [rad*(max1*np.cos(phi[i])+min1*np.sin(phi[i]))]
+        phis = 2*np.pi*(np.arange(nphi)+0.5)/nphi
+        startpts_fan = [rad*(max1*np.cos(phi[i]) + min1*np.sin(phi[i])) for phi in phis]
     
-    if ball == True:
-        rad = 5*rsphere/2e1
+    startpts_ball = []
+    if ball:
+        rad = rsphere*5/2e1
         ntheta = 6
         nphi = 6
-        theta = np.pi*((np.arange(ntheta)+0.5)/ntheta)
-        phi = 2*np.pi*((np.arange(nphi)+0.5)/nphi)
-        for it in theta:
-            for ip in phi:
-                xp = rad*np.cos(ip)*np.sin(it)
-                yp = rad*np.sin(ip)*np.sin(it)
-                zp = rad*np.cos(it)
-                startpts = startpts + [np.array([xp,yp,zp])]
+        thetas = np.pi*((np.arange(ntheta)+0.5)/ntheta)
+        phis = 2*np.pi*((np.arange(nphi)+0.5)/nphi)
+        startpts_ball = [np.array([rad*np.cos(ip)*np.sin(it), rad*np.sin(ip)*np.sin(it), rad*np.cos(it)]) for it in thetas for ip in phis]
+    
+    startpts = np.array([*startpts_fan, *startpts_ball])
 
-    if ball == True or fan == True:
+    if ball or fan:
         h0 = rsphere*h0
-        countf = 0
-        countb = 0
         for startpt in startpts:
             h = h0
-            line = fl.fieldline3d(startpt + nullpos, bgrid, xgc,ygc,zgc, h, 0.1*h, 10*h, 0.01*h, boxedge=boxedge, oneway=True, gridcoord=True, coordsystem=coordsystem)
-            plt.plot(line[:,0], line[:,1], line[:,2], c='red')
-            if (line[-1,0] < boxedge[0,0] + rsphere*1e-3 or line[-1,0] > boxedge[1,0] - rsphere*1e-3 or
-                line[-1,1] < boxedge[0,1] + rsphere*1e-3 or line[-1,1] > boxedge[1,1] - rsphere*1e-3 or
-                line[-1,2] < boxedge[0,2] + rsphere*1e-3 or line[-1,2] > boxedge[1,2] - rsphere*1e-3):
-                countf += 1
+            line = fl.fieldline3d(startpt + nullpos, bgrid, xgc,ygc,zgc, h, 0.1*h, 10*h, 0.01*h,
+                boxedge=boxedge, oneway=True, gridcoord=True, coordsystem=coordsystem,
+                stop_criteria=False, periodicity=None)
+            plt.plot(*line.T, c='red')
 
             h = h0
-            line = fl.fieldline3d(startpt + nullpos, bgrid, xgc,ygc,zgc, -h, 0.1*h, 10*h, 0.01*h, boxedge=boxedge, oneway=True, gridcoord=True, coordsystem=coordsystem)
-            plt.plot(line[:,0], line[:,1], line[:,2], c='orange')
-            if (line[0,0] < boxedge[0,0] + rsphere*1e-3 or line[0,0] > boxedge[1,0] - rsphere*1e-3 or
-                line[0,1] < boxedge[0,1] + rsphere*1e-3 or line[0,1] > boxedge[1,1] - rsphere*1e-3 or
-                line[0,2] < boxedge[0,2] + rsphere*1e-3 or line[0,2] > boxedge[1,2] - rsphere*1e-3):
-                countb += 1
-        
-        if badtest == True:
-            nlines = len(startpts)
-            print(n, countf, countb, nlines)
-            plt.close()
-            if (countf > 0.9*nlines and countb < 0.1*nlines) or (countf < 0.1*nlines and countb > 0.9*nlines):
-                print("Actually probs a bad null ({}) at {}".format(n, nullpos))
-                return n
-            else:
-                plt.close()
-        else:
-            plt.suptitle(r'File: {} Null: {}'.format(filename, n).replace('_', '\_'))
+            line = fl.fieldline3d(startpt + nullpos, bgrid, xgc,ygc,zgc, -h, 0.1*h, 10*h, 0.01*h,
+                boxedge=boxedge, oneway=True, gridcoord=True, coordsystem=coordsystem,
+                stop_criteria=False, periodicity=None)
+            plt.plot(*line.T, c='orange')
 
-            print("----------------------------------------")
-            print("Key:")
-            print("----")
-            print("Null - Red cross")
-            print("Spine - Dark blue cross")
-            print("Spine line - Blue line")
-            print("Maxvec - Cyan cross")
-            print("Minvec - Green cross")
-            print("Fan plane - Black circle")
-            print("Converged fan - Small black crosses")
-            print("Converged spine - Small red crosses")
-            print("Lines traced forwards - Red lines")
-            print("Lines traced backwards - Orange Lines")
-            print("----------------------------------------")
+        plt.suptitle(r'File: {} Null: {}'.format(filename, n).replace('_', r'\_'))
+
+    print("----------------------------------------")
+    print("Key:")
+    print("----")
+    print("Null - Red cross")
+    print("Spine - Dark blue cross")
+    print("Spine line - Blue line")
+    print("Maxvec - Cyan cross")
+    print("Minvec - Green cross")
+    print("Fan plane - Black circle")
+    print("Converged fan - Small black crosses")
+    print("Converged spine - Small red crosses")
+    print("Lines traced forwards - Red lines")
+    print("Lines traced backwards - Orange Lines")
+    print("----------------------------------------")
