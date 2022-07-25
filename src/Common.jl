9bfbffe3ff064e4eab1cde41305fb4880b61a725
Benjamin Williams <benmw@hotmail.co.uk>
2022-07-25 09:55:43 +0100

    No need to specialise on T

--- src/Common.jl
+++ src/Common.jl
@@ -62,12 +62,12 @@ module Common
 
     end
 
-    Base.minimum(a::Vector3D{T}) where T = min(a.x, a.y, a.z)
+    Base.minimum(a::Vector3D) = min(a.x, a.y, a.z)
 
     Base.zero(::Type{Vector3D{T}}) where T = Vector3D(zero(T), zero(T), zero(T))
 
     Base.read(io::IO, ::Type{Vector3D{T}}) where T = Vector3D{T}(read(io, T), read(io, T), read(io, T))
-    Base.write(io::IO, v::Vector3D{T}) where T = write(io, v.x) + write(io, v.y) + write(io, v.z)
+    Base.write(io::IO, v::Vector3D) = write(io, v.x) + write(io, v.y) + write(io, v.z)
     Base.show(io::IO, a::Vector3D) = print(io, "Vector3D[$(a.x), $(a.y), $(a.z)]")
 
     abstract type AbstractField3D end
